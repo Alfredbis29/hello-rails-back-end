45bb44559194a1ee31b22cbc5dcacfffecf0d018
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2022-09-27 09:54:21 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -6,7 +6,7 @@
 # db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application co.
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
